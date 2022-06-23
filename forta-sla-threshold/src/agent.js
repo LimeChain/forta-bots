e617317980482220564715751853e4cd783c30eb
TDrachev <drachev.td@gmail.com>
2022-06-23 13:26:28 +0300

    Reworked SLA threshold bot

--- forta-sla-threshold/src/agent.js
+++ forta-sla-threshold/src/agent.js
@@ -7,113 +7,86 @@ const {
 } = require("forta-agent");
 const axios = require("axios");
 const { config } = require("./agent.config");
-const { Contract, Provider } = require("ethers-multicall");
+const { Provider, Contract } = require("ethers-multicall");
 const ADDRESS_ZERO = ethers.constants.AddressZero;
-
-const agentRegistryAbi = require("./abis/agentregistry.json");
-const dispatcherAbi = require("./abis/dispatcher.json");
-
 const mintAbi =
   "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)";
-
+const totalScannersPerQuery = 900; //Max scanners per query cannot exceed 900
 const provider = getEthersProvider();
-
-const contractAddresses = Object.keys(config.contracts);
 const ethcallProvider = new Provider(provider);
-const agentRegistryContract = new Contract(
-  contractAddresses[1],
-  agentRegistryAbi
+const scannerABI = require("./abis/scannerregistry.json");
+const scannerRegistry = new Contract(
+  config.scannerRegistryContract,
+  scannerABI
 );
-const dispatcherContract = new Contract(contractAddresses[0], dispatcherAbi);
 let scannersLoaded = [];
 let alerts = [];
 let isChecking = false;
 let lastCheck = 0;
 
-async function initialize() {
-  await ethcallProvider.init();
-
-  //Here we loop trough all available chain ids and fetch all the scannerIds that are currently available
-  for (let id of config.chainIds) {
-    const agentCountCall = agentRegistryContract.getAgentCountByChain(id);
-    const [agentCountFetched] = await ethcallProvider.all([agentCountCall]);
-
-    //Agent count as number
-    const agentCount = agentCountFetched.toNumber();
-    const agentCalls = [];
-    for (let i = 0; i < agentCount; i++) {
-      agentCalls.push(agentRegistryContract.getAgentByChainAndIndex(id, i));
-    }
-
-    //All agents
-    let agents = await ethcallProvider.all(agentCalls);
-
-    const agentDataCalls = [];
-    agents.forEach((a) => {
-      agentDataCalls.push(agentRegistryContract.isEnabled(a));
-    });
-
-    //Remove disabled agents from the list since they are not located on a scanner eitherway
-    const agentEnabled = await ethcallProvider.all(agentDataCalls);
-
-    agents = agents.filter((a, i) => {
-      if (agentEnabled[i] == true) return a;
-    });
-
-    const numScannersForCalls = agents.map((a) =>
-      dispatcherContract.numScannersFor(a)
-    );
-
-    //Get all scanner count for a specific agent
-    const numScannersFor = await ethcallProvider.all(numScannersForCalls);
-
-    //Get scanners by getting the total scanners for an agent and then looping trough each agent and pushing the scannerAt call for later
-    const scannerCalls = [];
-    numScannersFor
-      .map((e) => e.toNumber())
-      .forEach((num, agentIndex) => {
-        for (let i = 0; i < num; i++) {
-          scannerCalls.push(
-            dispatcherContract.scannerAt(agents[agentIndex], i)
-          );
-        }
-      });
-
-    //Get all scanners
-    const scanners = await ethcallProvider.all(scannerCalls);
-
-    //Convert the scannerIds to strings since they overflow as numbers
-    const scannersAsStrings = scanners.map((s) => s.toString());
-
-    //Filter out duplicate scanners
-    const scannersFiltered = scannersAsStrings.filter((s, i) => {
-      return scannersAsStrings.indexOf(s) === i;
-    });
-
-    //Put all found scanners in the global scannersLoaded object
-    scannersFiltered.forEach((s) => {
-      scannersLoaded.push(ethers.BigNumber.from(s));
-    });
+async function fetchScannersFromGraph(skip, array) {
+  if (totalScannersPerQuery > 900) {
+    throw new Error("The subgraph query cannot exceed 900");
   }
 
-  //Filter all scanners just in case there are duplicates
-  scannersLoaded = scannersLoaded.filter((s, i) => {
-    return scannersLoaded.indexOf(s) === i;
+  const query = `{
+    scanners (first:${totalScannersPerQuery} skip:${skip}){
+          id
+    }
+  }`;
+
+  const fetched = await axios.default.post(config.subgraphURL, {
+    query,
   });
+  const res = fetched.data.data;
+  array = [...array, ...res.scanners];
+  if (res.scanners.length < totalScannersPerQuery) {
+    const arr = [];
+    array.reduce((prev, curr) => {
+      prev.push({
+        id: curr.id,
+        chainId: curr.chainId,
+      });
+      return prev;
+    }, arr);
+    return arr;
+  }
+  return fetchScannersFromGraph(skip + totalScannersPerQuery, array);
+}
+
+async function initialize() {
+  await ethcallProvider.init();
+  let scanners = await fetchScannersFromGraph(0, []);
+  const scannerEnabledCalls = [];
+  scanners.forEach((s) => {
+    scannerEnabledCalls.push(
+      scannerRegistry.isEnabled(ethers.BigNumber.from(s.id).toString())
+    );
+  });
+  const scannersEnabled = await ethcallProvider.all(scannerEnabledCalls);
+  scanners = scanners.filter((s, i) => {
+    return scannersEnabled[i];
+  });
+  for (let scanner of scanners) {
+    scannersLoaded.push(scanner);
+  }
 }
 
 //Here we check if a new scanner is minted so we can add it to the array of scanners
 function provideHandleTransaction(scannersLoaded) {
   return async function handleTransaction(txEvent) {
     const findings = [];
-    const txFiltered = txEvent.filterLog(mintAbi, contractAddresses[2]);
+    const txFiltered = txEvent.filterLog(
+      mintAbi,
+      config.scannerRegistryContract
+    );
 
     for (let tx of txFiltered) {
       const { from, tokenId } = tx.args;
 
       if (from === ADDRESS_ZERO) {
-        const tokenIdString = tokenId.toString();
-        scannersLoaded.push(tokenIdString);
+        const tokenIdString = tokenId.toHexString();
+        scannersLoaded.push({ id: tokenIdString });
       }
     }
 
@@ -138,9 +111,9 @@ function provideHandleBlock(scannersLoaded) {
 
 async function runJob(scannersLoaded) {
   isChecking = true;
-
+  console.log("CHECKING");
   for (let s of scannersLoaded) {
-    const scannerAddress = s.toHexString();
+    const scannerAddress = s.id;
 
     try {
       const scannerSlaData = await axios.default.get(
@@ -153,7 +126,7 @@ async function runJob(scannersLoaded) {
         alerts.push(
           Finding.fromObject({
             name: "Scanner SLA under threshold",
-            description: `Scanner SLA is under the threshold and might get disqualifed, scannerId: ${s}`,
+            description: `Scanner SLA is under the threshold and might get disqualifed, scannerId: ${s.id}`,
             alertId: "FORTA-SCANNER-SLA-UNDER-THRESHOLD",
             severity: FindingSeverity.High,
             type: FindingType.Info,
